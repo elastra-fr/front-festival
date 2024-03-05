962afc6f7d3498978ce68f1449ba7dfd30b358c1
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-06 00:57:40 +0100

    test header animé

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
+
+
+
 
 const Header = () => {
     return (
         <header>
-            <h1>Nation Sound</h1>
+
+<div className='videoWrapper'>
+    <video id="headerVideo" loop src="/video/video1.mp4" autoPlay muted></video>
+    <div id='headerTitle'><h1>Nation Sound</h1></div>
+</div>
+
+
+            
+
             <ul>
                 <li><Link>Accueil</Link></li>
                 <li></li>
