252a0695b9d2c26d18c84662c43941176d171ed0
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-06 16:37:59 +0100

    style header + nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-
+import Socials from '../socials/Socials';
 
 
 
 const Header = () => {
     return (
         <header>
-
 <div className='videoWrapper'>
-    <video id="headerVideo" loop src="/video/video1.mp4" autoPlay muted></video>
-    <div id='headerTitle'><h1>Nation Sound</h1></div>
+            <nav id="mainNav"><ul>
+                <li><Link>Accueil</Link></li>
+                <li>Actualité</li>
+                <li>Informations</li>
+                <li>Programmation</li>
+            </ul>
+        </nav>
+
+
+    <video id="headerVideo" loop src="/video/video2.mp4" autoPlay muted></video>
+    <div id='headerTitle'><h1>NATION SOUND</h1>
+    <Socials />
+    
+    <input id= "ctaBilleterie" type='button' value="Billetterie"/>
+    
+    </div>
+    <div id="headerMusic">
+
+    </div>
 </div>
 
 
             
 
-            <ul>
-                <li><Link>Accueil</Link></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+
         </header>
     );
 };
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,12 +1,12 @@
 //import React from 'react';
-import Socials from '../../components/socials/Socials';
+//import Socials from '../../components/socials/Socials';
 import Header from '../../components/header/Header';
 
 const HomePage = () => {
     return (
         <>
         <Header/>
-        <Socials/>
+
         </>
     );
 };
