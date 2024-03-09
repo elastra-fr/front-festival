7debffb984bfb1cb719fda9717e4b634b5f284a1
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-09 01:27:45 +0100

    structure élément map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -3,7 +3,44 @@ import React from 'react';
 const Map = () => {
     return (
         <div>
-            {/* Your map component content goes here */}
+                <div className="mapWrapper">
+
+                    <h2>Carte interactive</h2>
+                    <div className='mapTools'>
+
+                        h3>Filtrer par :</h3>
+                        <select name="filtremap" id="filtremap">
+                            <option value="Tout">Tout</option>
+                            <option value="Scènes">Scènes</option>
+                            <option value="Buvettes">Buvettes</option>
+                            <option value="Restauration">Restauration</option>
+                            <option value="WC">WC</option>
+                            <option value="Accueil">Accueil</option>
+                            <option value="Camping">Camping</option>
+                            <option value="Parking">Parking</option>
+                            <option value="Accès PMR">Accès PMR</option>
+                            <option value="Sécurité">Sécurité</option>
+                            <option value="Secours">Secours</option>
+                            <option value="Boutique">Boutique</option>
+                            <option value="Animations">Animations</option>
+                            <option value="Espace VIP">Espace VIP</option>
+                            
+
+
+
+                            </select>
+
+<div className="map">
+        
+    
+    
+    
+    </div>
+
+
+                        </div>
+
+                    </div>
         </div>
     );
 };
