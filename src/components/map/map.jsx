a69dc51780acbe2cf4e9ec097e9711ececfba206
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-10 01:35:39 +0100

    lien api pour les map points et injection sur la map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Map.css';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+
 const Map = () => {
 
-
+const [mapPoints, setMapPoints] = React.useState([]); 
 
 const mapKey = import.meta.env.VITE_FESTIVAL_APP_API_MAP;
+
+//Fonction pour récupérer les données de la carte
+
+
+function getMapData() {
+    fetch("http://festival.local/wp-json/wp/v2/mappoints")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[0].acf.Lieu);
+        setMapPoints(data);
+        
+        //length = nombre d'éléments dans le tableau
+        console.log(data.length);
+        
+    });
+
+}
+
+
+useEffect(() => { 
+getMapData();
+initMap();
+} , []);
+
+useEffect(() => {
+    initMap();
+}, [mapPoints]);
+
+
+
 
 let map;
 
@@ -21,22 +53,63 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     zoom: 15,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "DEMO_MAP_ID", 
     mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain"],
   });
 
-  // The marker, positioned at Uluru
+
+console.log(mapPoints);
+
+console.log(mapPoints[0].acf.Lieu.lat);
+
+//Affiche les marqueurs sur la carte
+for (let i = 0; i < mapPoints.length; i++) {
+    const marker = new AdvancedMarkerView({
+        map: map,
+        position: { lat: mapPoints[i].acf.Lieu.lat, lng: mapPoints[i].acf.Lieu.lng }
+    });
+
+    marker.addListener("click", () => {
+        const infowindow = new google.maps.InfoWindow({
+            content: "<h3>" + mapPoints[i].acf.Lieu + "</h3><p>" + mapPoints[i].acf.Description + "</p>",
+        });
+        infowindow.open(map, marker);
+    });
+}
+
+/*
+const mark1= new AdvancedMarkerView({
+    map: map,
+    position: { lat: 48.76890, lng: 2.092 },
+    title: "Festival Nation Sound",
+});*/
+
+
+
+
   const marker = new AdvancedMarkerView({
     map: map,
     position: position,
     title: "Festival Nation Sound",
   });
 
+
+
 map.setMapTypeId('satellite');
+
+//Affiche infos quand on clique sur un marqueur
+marker.addListener("click", () => {
+    const infowindow = new google.maps.InfoWindow({
+        content: "<h3>Festival Nation Sound</h3><p>Le festival de musique reggae de la région parisienne</p>",
+    });
+    infowindow.open(map, marker);
+});
+
 
 }
 
-initMap();
+
+
 
 
 
