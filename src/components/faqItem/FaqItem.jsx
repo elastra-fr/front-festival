2bf439996f07c601b90af4895f8b08c4d92c1a9e
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-11 17:28:04 +0100

    footer responsive

--- src/components/faqItem/FaqItem.jsx
+++ src/components/faqItem/FaqItem.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-
-const FaqItem = ({titre, contenu}) => {
-    return (
-<>
-<details>
-<summary><h3>{titre}</h3></summary>
-<ul>
-
-{contenu.map((item, index) => {
-    return <li key={index}>{item}</li>;
-})}
-</ul>
-</details>
-
-
-</>
-    );
+import React from "react";
+import "./FaqItem.css";
+const FaqItem = ({ titre, contenu }) => {
+  return (
+    <>
+      <details className="faqItemWrapper">
+        <summary>
+          <h3>{titre}</h3>
+        </summary>
+        <ul className="faqItemList">
+          {contenu.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </details>
+    </>
+  );
 };
 
 export default FaqItem;
--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -12,7 +12,8 @@ const Faq = () => {
 <main className='mainFaq'>
 
 <FaqItem titre='Dates et Lieu' contenu={['Dates du Festival : [Insérer les dates du festival ici]' , 'Lieu : [Insérer le lieu du festival ici]', "Adresse : [Insérer l'adresse complète du lieu du festival ici]" ]} />
-
+<FaqItem titre="Horaires" contenu={["Heures d'ouverture du Festival : Les portes ouvrent à [insérer l'heure d'ouverture] chaque jour du festival.", "Horaires des Performances : Consultez l'horaire complet des performances sur notre site web et sur les panneaux d'affichage du festival."]} />
+<FaqItem titre="Accès au festival" contenu={["Transport en Commun : [Insérer les informations sur les transports en commun disponibles, y compris les arrêts de bus ou de train les plus proches du site du festival.]", "Parking : Des options de stationnement seront disponibles à proximité du site du festival. Veuillez suivre les indications pour le stationnement.", "Navettes : Des navettes gratuites seront mises à disposition pour certains parkings éloignés. Veuillez consulter notre site web pour plus de détails."]} />
 
 
 
