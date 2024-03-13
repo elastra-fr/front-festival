174841c160f9a86b9198c7a892aacb69ea08d15d
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-03-13 16:54:05 +0100

    optimisation header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,39 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import Socials from "../socials/Socials";
 import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
 
 const Header = () => {
+
+  const [showNav, setShowNav] = useState(false);
+
+  const getWindowWidth = () => {
+
+    return window.innerWidth;
+
+  }
+
+const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+
+  const handleShowNav = () => {
+    setShowNav(!showNav);
+  };
+
+
+
+  window.addEventListener('resize', () => {
+    setWindowWidth(getWindowWidth());
+  });
+
+
+
+
+
   return (
     <header>
       <div className="videoWrapper">
-        <nav id="mainNav">
+        <nav id="mainNav" className={`${showNav ? "showNav" : "hideNav"}`}>
           <ul>
             <li>
               <Link to="/home">Accueil</Link>
@@ -23,13 +50,27 @@ const Header = () => {
           </ul>
         </nav>
 
-        <video
+  {windowWidth > 768 ? 
+  <video
           id="headerVideo"
           loop
           src="/video/video2.mp4"
           autoPlay
           muted
-        ></video>
+        />
+  : 
+
+   <img id="headerImg"
+        src="/images/header-mobile.avif"
+        alt="Une personne qui danse sur un fond de couleur bleu nuit."
+        
+        
+        ></img>}
+ 
+  
+         
+
+       
         <div id="headerTitle">
           <h1>NATION SOUND</h1>
           <Socials />
@@ -37,6 +78,19 @@ const Header = () => {
           <input id="ctaBilleterie" type="button" value="Billetterie" />
         </div>
       </div>
+
+       <div className="burgerMenu" onClick={handleShowNav}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48px"
+            width="48px"
+            viewBox="0 0 448 512"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </svg>
+        </div>
+
+
     </header>
   );
 };
